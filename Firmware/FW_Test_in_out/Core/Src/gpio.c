7b11d2090c379817a0f33db82bc6d05c549a4428
TaoTran224 <trannhutao.224@gmail.com>
2026-06-06 09:30:54 +0700

    Init firmware

---
+++ Firmware/FW_Test_in_out/Core/Src/gpio.c
@@ -0,0 +1,107 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, OUT_P_0_Pin|OUT_P_1_Pin|OUT_P_2_Pin|ENB_485_1_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(OUT_P_3_GPIO_Port, OUT_P_3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(OUT_N_0_GPIO_Port, OUT_N_0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, OUT_N_1_Pin|ENB_485_3_Pin|OUT_N_2_Pin|OUT_N_3_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : LED_RUN_Pin */
+  GPIO_InitStruct.Pin = LED_RUN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(LED_RUN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : OUT_P_0_Pin OUT_P_1_Pin OUT_P_2_Pin OUT_P_3_Pin
+                           ENB_485_1_Pin */
+  GPIO_InitStruct.Pin = OUT_P_0_Pin|OUT_P_1_Pin|OUT_P_2_Pin|OUT_P_3_Pin
+                          |ENB_485_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : OUT_N_0_Pin OUT_N_1_Pin ENB_485_3_Pin OUT_N_2_Pin
+                           OUT_N_3_Pin */
+  GPIO_InitStruct.Pin = OUT_N_0_Pin|OUT_N_1_Pin|ENB_485_3_Pin|OUT_N_2_Pin
+                          |OUT_N_3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : INPUT_0_Pin */
+  GPIO_InitStruct.Pin = INPUT_0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(INPUT_0_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : INPUT_1_Pin INPUT_2_Pin INPUT_3_Pin INPUT_4_Pin
+                           INPUT_5_Pin */
+  GPIO_InitStruct.Pin = INPUT_1_Pin|INPUT_2_Pin|INPUT_3_Pin|INPUT_4_Pin
+                          |INPUT_5_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
