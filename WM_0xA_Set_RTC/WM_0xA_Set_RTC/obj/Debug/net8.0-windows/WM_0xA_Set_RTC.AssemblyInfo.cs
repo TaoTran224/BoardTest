725732e3bfe2e4d5e441a71567e3a064e22c1f37
TRAN TAO <trannhutao.224@gmail.com>
2026-09-15 23:40:45 +0700

    update daily

--- WM_0xA_Set_RTC/WM_0xA_Set_RTC/obj/Debug/net8.0-windows/WM_0xA_Set_RTC.AssemblyInfo.cs
+++ WM_0xA_Set_RTC/WM_0xA_Set_RTC/obj/Debug/net8.0-windows/WM_0xA_Set_RTC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WM_0xA_Set_RTC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e2b588a3d9023aa65f929a174df2adaeaccb6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9a318513a8b7cd0a45a2b4d0cd054a1e1a3a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WM_0xA_Set_RTC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WM_0xA_Set_RTC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
